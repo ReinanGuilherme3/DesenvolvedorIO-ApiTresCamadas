a0fc5ea6c531d4f2a92b9e1b446d7b43d6bfa525
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2026-01-25 15:58:00 -0300

    Definindo interfaces de persistência

---
+++ src/DevIO.Domain/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,11 @@
+﻿using DevIO.Domain.Entities;
+
+namespace DevIO.Domain.Interfaces;
+
+public interface IFornecedorRepository : IRepository<Fornecedor>
+{
+    Task<Fornecedor> ObterFornecedorEndereco(Guid id);
+    Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id);
+
+    Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+}
---
+++ src/DevIO.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using DevIO.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace DevIO.Domain.Interfaces;
+
+public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+{
+    Task Adicionar(TEntity entity);
+    Task Atualizar(TEntity entity);
+    Task Remover(Guid id);
+    Task<TEntity> ObterPorId(Guid id);
+    Task<IEnumerable<TEntity>> ObterTodos();
+    Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+    Task<int> SaveChanges();
+}
