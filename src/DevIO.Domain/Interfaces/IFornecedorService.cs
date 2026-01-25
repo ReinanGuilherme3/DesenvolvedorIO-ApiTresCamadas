3b5a956d0a83d562001f721044ab44b1b36abab7
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2026-01-25 15:42:10 -0300

    Definindo interfaces de serviços

---
+++ src/DevIO.Domain/Interfaces/IFornecedorService.cs
@@ -0,0 +1,10 @@
+﻿using DevIO.Domain.Entities;
+
+namespace DevIO.Domain.Interfaces;
+
+public interface IFornecedorService : IDisposable
+{
+    Task Adicionar(Fornecedor entity);
+    Task Atualizar(Fornecedor entity);
+    Task Remover(Guid id);
+}
---
+++ src/DevIO.Domain/Interfaces/IProdutoService.cs
@@ -0,0 +1,10 @@
+﻿using DevIO.Domain.Entities;
+
+namespace DevIO.Domain.Interfaces;
+
+public interface IProdutoService : IDisposable
+{
+    Task Adicionar(Produto entity);
+    Task Atualizar(Produto entity);
+    Task Remover(Guid id);
+}
