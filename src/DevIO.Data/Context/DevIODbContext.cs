7c5a018533442e01fd81ec77ec6276930bdac41f
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2026-01-27 22:12:44 -0300

    Definindo o contexto do Entity Framework

---
+++ src/DevIO.Data/Context/DevIODbContext.cs
@@ -0,0 +1,11 @@
+﻿using DevIO.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context;
+
+public class DevIODbContext(DbContextOptions<DevIODbContext> options) : DbContext(options)
+{
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Fornecedor> Fornecedores { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+}
