4f908a3c09b4f77b139ac40cf8499e9a117d8363
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2026-01-30 19:37:16 -0300

    Customizando comportamentos do EF

--- src/DevIO.Data/Context/DevIODbContext.cs
+++ src/DevIO.Data/Context/DevIODbContext.cs
@@ -3,8 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DevIO.Data.Context;
 
-public class DevIODbContext(DbContextOptions<DevIODbContext> options) : DbContext(options)
+public class DevIODbContext : DbContext
 {
+    public DevIODbContext(DbContextOptions<DevIODbContext> options) : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
+    }
+
     public DbSet<Produto> Produtos { get; set; }
     public DbSet<Fornecedor> Fornecedores { get; set; }
     public DbSet<Endereco> Enderecos { get; set; }
@@ -20,5 +26,19 @@ public class DevIODbContext(DbContextOptions<DevIODbContext> options) : DbContex
             property.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+        {
+            if (entry.State == EntityState.Added)
+                entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+
+            if (entry.State == EntityState.Modified)
+                entry.Property("DataCadastro").IsModified = false;
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
