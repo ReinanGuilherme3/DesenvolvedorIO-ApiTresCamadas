a6d9f9e282d4988b54a5ee0e01d0d9465fb609d6
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2026-02-02 19:30:16 -0300

    resolvendo dependencias

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -1,10 +1,12 @@
 using DevIO.Api.Configurations;
 using DevIO.Data;
+using DevIO.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApiConfiguration();
 builder.Services.AddSwaggerConfiguration();
+builder.Services.AddDomain();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
---
+++ src/DevIO.Domain/DependencyInjectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.Domain;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddDomain(this IServiceCollection services)
+    {
+        AddDomainServices(services);
+    }
+    public static void AddDomainServices(IServiceCollection services)
+    {
+        services.Scan(scan => scan
+            .FromAssembliesOf(typeof(DependencyInjectionExtension))
+            .AddClasses(classes => classes.InNamespaces("DevIO.Domain.Services"))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+    }
+}
